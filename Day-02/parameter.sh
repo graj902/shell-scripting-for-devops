4cbfdabae9e89b4dc337c16b5a6fce43b124f50c
Ubuntu <ubuntu@ip-172-31-37-57.ap-south-1.compute.internal>
2025-09-26 08:39:00 +0000

    this is user and date commit

---
+++ Day-02/parameter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# comment this is multiple commnent 
+# we can also use it
+#comment/n
+
+name="Babitaji"
+echo  "the name is $name and date is $(date)"
+echo "enter name"
+read   username
+echo "enter the user name $username"
+echo "you entered the $0 and $1 $2"
+
